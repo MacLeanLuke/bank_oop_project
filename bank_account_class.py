d979562f00e268e3be5d9b20cb87e7fc06dea3ae
Luke MacLean <40969439+MacLeanLuke@users.noreply.github.com>
2021-07-08 09:02:07 -0500

    users with bank accounts

--- bank_account_class.py
+++ bank_account_class.py
@@ -1,3 +1,5 @@
+# from bank_user_class import User
+
 # The BankAccount class should have a balance. When a new BankAccount instance is created, if an amount is given, the balance of the account should initially be set to that amount; otherwise, the balance should start at $0. The account should also have an interest rate, saved as a decimal (i.e. 1% would be saved as 0.01), which should be provided upon instantiation. (Hint: when using default values in parameters, the order of parameters matters!)
 
 class BankAccount:
@@ -8,12 +10,12 @@ class BankAccount:
         self.balance = 0
 
         # don't worry about user info here; we'll involve the User class soon
-    def make_deposit(self, amount):
+    def deposit(self, amount):
         # deposit(self, amount) - increases the account balance by the given amount
         # your code here
         self.balance += amount
 
-    def make_withdrawal(self, amount):
+    def withdraw(self, amount):
         # withdraw(self, amount) - decreases the account balance by the given amount if there are sufficient funds; if there is not enough money, print a message "Insufficient funds: Charging a $5 fee" and deduct $5
         # your code here
         self.balance -= amount
@@ -29,24 +31,24 @@ class BankAccount:
         self.balance += (self.balance * (1 + self.int_rate))
 
 # Create 2 accounts
-account1 = BankAccount(.02, 5)
-account2 = BankAccount(.03, 10)
+# account1 = BankAccount(.02, 5)
+# account2 = BankAccount(.03, 10)
 
 # To the first account, make 3 deposits and 1 withdrawal, then yield interest and display the account's info all in one line of code (i.e. chaining)
-account1.make_deposit(100)
-account1.make_deposit(500)
-account1.make_deposit(2)
-account1.make_withdrawal(50)
-account1.yield_interest()
-account1.display_account_info()
+# account1.make_deposit(100)
+# account1.make_deposit(500)
+# account1.make_deposit(2)
+# account1.make_withdrawal(50)
+# account1.yield_interest()
+# account1.display_account_info()
 
 # To the second account, make 2 deposits and 4 withdrawals, then yield interest and display the account's info all in one line of code (i.e. chaining)
-account2.make_deposit(10000)
-account2.make_deposit(500)
-account2.make_withdrawal(50)
-account2.make_withdrawal(5)
-account2.make_withdrawal(5000)
-account2.make_withdrawal(150)
-account2.yield_interest()
-account2.display_account_info()
+# account2.make_deposit(10000)
+# account2.make_deposit(500)
+# account2.make_withdrawal(50)
+# account2.make_withdrawal(5)
+# account2.make_withdrawal(5000)
+# account2.make_withdrawal(150)
+# account2.yield_interest()
+# account2.display_account_info()
 
--- bank_user_class.py
+++ bank_user_class.py
@@ -1,26 +1,35 @@
 # tried to add chaining but I'm not sure it what isn't working
 # received error: AttributeError: 'NoneType' object has no attribute 'make_deposit'
 
+# Take a look into modularization if you feel the need to have the 2 classes in separate files.
+from bank_account_class import BankAccount
+
+
 # methods must be called from an instance of a class. 
 class User:		# here's what we have so far
     def __init__(self, name, email):
         self.name = name
         self.email = email
-        self.account_balance = 0
+        # Update the User class __init__ method
+        self.account = BankAccount(int_rate=0.02, balance=0)
+
     # adding the deposit method
-    def make_deposit(self, amount):	# takes an argument that is the amount of the deposit
-    	self.account_balance += amount	# the specific user's account increases by the amount of the value received
+    # Update the User class make_deposit method
+    def user_deposit_method(self, amount):	# takes an argument that is the amount of the deposit
+    	self.account.deposit(amount)	# the specific user's account increases by the amount of the value received
 
-    def make_withdrawal(self, amount):
-        self.account_balance -= amount
+    # Update the User class make_withdrawal method
+    def user_withdraw_method(self, amount):
+        self.account.withdraw(amount)
 
-    def display_user_balance(self):
-        return f"Account Holder: {self.name}, has a balance of: ${self.account_balance}"
+    # Update the User class display_user_balance method
+    def user_display_balance_method(self):
+        return f"Account Holder: {self.name}, has a balance of: ${self.account.display_account_info()}"
 
     def transfer_money(self, other_user, amount):
-        if amount >=0 and amount < self.account_balance:
-            self.account_balance = self.account_balance - amount
-            other_user.make_deposit(amount)
+        if amount >=0 and amount < self.account.balance:
+            self.account.balance = self.account.balance - amount
+            other_user.account.deposit(amount)
 
 # Create 3 instances of the User class
 charles = User("Charles van Rotherdam", "charlie@pmail.com")
@@ -28,30 +37,30 @@ guido = User("Guido van Rossum", "guido@python.com")
 fido = User("fido van Rossum", "fido@python.com")
 
 # Have the first user make 3 deposits and 1 withdrawal and then display their balance
-charles.make_deposit(100)
-charles.make_deposit(500)
-charles.make_deposit(2)
-charles.make_withdrawal(50)
-print(charles.display_user_balance())
+charles.user_deposit_method(100)
+charles.user_deposit_method(500)
+charles.user_deposit_method(2)
+charles.user_withdraw_method(50)
+print(charles.user_display_balance_method())
 
 # Have the second user make 2 deposits and 2 withdrawals and then display their balance
-guido.make_deposit(9)
-guido.make_deposit(956)
-guido.make_withdrawal(50)
-guido.make_withdrawal(10)
-print(guido.display_user_balance())
+guido.user_deposit_method(9)
+guido.user_deposit_method(956)
+guido.user_withdraw_method(50)
+guido.user_withdraw_method(10)
+print(guido.user_display_balance_method())
 
 
 # Have the third user make 1 deposits and 3 withdrawals and then display their balance
-fido.make_deposit(10000)
-fido.make_withdrawal(50)
-fido.make_withdrawal(50)
-fido.make_withdrawal(2)
-print(fido.display_user_balance())
+fido.user_deposit_method(10000)
+fido.user_withdraw_method(50)
+fido.user_withdraw_method(50)
+fido.user_withdraw_method(2)
+print(fido.user_display_balance_method())
 
 # BONUS: Add a transfer_money method; have the first user transfer money to the third user and then print both users' balances
-charles.transfer_money(fido, 10)
-print(charles.display_user_balance())
-print(fido.display_user_balance())
+# charles.transfer_money(fido, 10)
+# print(charles.display_user_balance())
+# print(fido.display_user_balance())
 
 # Because we are calling on the method from the instance, this is known as implicit passage of self. When we call on a method from an instance, that instance, along with all of its information (name, email, balance), is passed in as self.
